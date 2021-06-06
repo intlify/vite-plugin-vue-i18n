e4658c43b8571b197d369ad683e56e9e6261be3a
kazuya kawaguchi <kawakazu80@gmail.com>
2021-06-07 03:28:30 +0900

    improve @intlify/vite-plugin-vue-i18n/messages typing (#104)

--- client.d.ts
+++ client.d.ts
@@ -1,6 +1,8 @@
 declare module '@intlify/vite-plugin-vue-i18n/messages' {
   import { LocaleMessages } from '@intlify/core-base'
   import { VueMessageType } from 'vue-i18n'
-  const messages: LocaleMessages<VueMessageType>
+  export interface BundleLocaleMessages
+    extends LocaleMessages<VueMessageType> {}
+  const messages: BundleLocaleMessages
   export default messages
 }
